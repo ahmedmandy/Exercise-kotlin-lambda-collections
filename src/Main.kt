781fee41e78304fe2ba2cbf01ae3f9c1b6af84b7
ahmedmandy <ahmedmandy@me.com>
2025-02-13 14:27:55 +0300

    Completed Part 2.

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,11 @@
+fun filterNames(list: List<String>, isTrue: (String) -> Boolean): List<String> {
+    return list.filter(isTrue)
+}
+
 fun main() {
     val integers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val integersDoubled = integers.map { it * 2 }
     println(integersDoubled)
+    val listOfNames = listOf("Alice", "Bob", "Amir", "Charlie", "Annie", "David")
+    println(filterNames(listOfNames, {it.first() == 'A'}))
 }
