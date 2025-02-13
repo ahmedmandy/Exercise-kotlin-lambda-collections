537c486c7f24e223ac3889fa2fb09892ae228118
ahmedmandy <ahmedmandy@me.com>
2025-02-13 22:19:55 +0300

    Completed bonus.

--- src/Main.kt
+++ src/Main.kt
@@ -10,6 +10,10 @@ fun processNumbers(numbers: List<Int>): List<Int> {
     return numbers.filter { it % 2 != 0 }.map { it * it }
 }
 
+fun processNumbersMod(numbers: List<Int>, filters: (Int) -> Boolean, transforms: (Int) -> Int): List<Int> {
+    return numbers.filter(filters).map(transforms)
+}
+
 fun main() {
     val integers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val integersDoubled = integers.map { it * 2 }
@@ -23,4 +27,5 @@ fun main() {
     println(customFilter(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) { it % 2 == 0 })
     println(customFilter(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) { it % 3 == 0 })
     println(processNumbers(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))
+    println(processNumbersMod(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), filters = { it % 2 != 0 }, transforms = { it * it }))
 }
